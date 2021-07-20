80d730bef5766638e0f86bf274e5656994c53d2e
Harshit Shah <harshits@kuberdigital.com>
2021-07-20 23:06:44 +0530

    redux setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import GameController from "./Components/GameController";
+import React, { Suspense } from "react";
+import { Provider } from "react-redux";
+import store, { history } from "./store";
+import { Route, Switch } from "react-router-dom";
+import { ConnectedRouter } from "connected-react-router";
 import "./Styles.css";
+
+const GameController = React.lazy(() => import("Components/GameController"));
 
 function App() {
   return (
-    <div className="App">
-      <GameController />
-    </div>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Suspense fallback={<div>Loading....</div>}>
+          <div className="App">
+            <Switch>
+              <Route exact path="/" component={GameController} />
+            </Switch>
+          </div>
+        </Suspense>
+      </ConnectedRouter>
+    </Provider>
   );
 }
 
