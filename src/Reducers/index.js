86df0ffd449542725de73c4a5c58537834cc2a24
Harshit Shah <harshits@kuberdigital.com>
2021-07-24 22:05:08 +0530

    actions and reducers for saving user last move

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,11 +1,12 @@
 // reducers.js
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
+import BoardReducer from "./boardReducer";
 
 const createRootReducer = (history) =>
   combineReducers({
     router: connectRouter(history),
-
+    board: BoardReducer,
     // rest of your reducers
   });
 export default createRootReducer;
